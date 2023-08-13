30e7773d8535523b6cb5333d606be2ecd610d79f
Angel Chira <125518726+AngelChira@users.noreply.github.com>
2023-08-13 00:04:11 -0500

    Mejora de la activity6

--- app/src/main/java/com/example/maquetacion/ui/MainActivity6.kt
+++ app/src/main/java/com/example/maquetacion/ui/MainActivity6.kt
@@ -1,12 +1,21 @@
 package com.example.maquetacion.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.maquetacion.R
 
 class MainActivity6 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main6)
+        val btnRegresarMenu = findViewById<Button>(R.id.btnIrMenu)
+
+        btnRegresarMenu.setOnClickListener{
+            val intent = Intent(this, MainActivity4::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
