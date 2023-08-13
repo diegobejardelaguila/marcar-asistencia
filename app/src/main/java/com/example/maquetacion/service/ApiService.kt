82edafea314270c14e137cc1c779587fdce80118
Luis Carcamo <138547371+LuisCarcam0@users.noreply.github.com>
2023-08-13 00:58:50 -0500

    actualizacion de datos

--- app/src/main/java/com/example/maquetacion/service/ApiService.kt
+++ app/src/main/java/com/example/maquetacion/service/ApiService.kt
@@ -1,6 +1,7 @@
 package com.example.maquetacion.service
 
 import android.content.Context
+import com.example.maquetacion.io.response.AsistenciaCreateResponse
 import com.example.maquetacion.io.response.AsistenciaResponse
 import com.example.maquetacion.model.login.LoginResponse
 import okhttp3.OkHttpClient
@@ -13,6 +14,7 @@ import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Query
+import com.example.maquetacion.model.Asistencia
 
 interface ApiService {
 
@@ -33,12 +35,8 @@ interface ApiService {
     fun getAsistencia(@Query("id") id: Int): Call<com.example.maquetacion.model.Asistencia>
 
     @POST("asistencia/")
-    @FormUrlEncoded
     fun postAsistencia(
-        @Field("fecha") fecha: String?,
-        @Field("hora") hora: String?,
-        @Field("tipo") tipo: String?,
-    ): Call<com.example.maquetacion.model.Asistencia>
+    ): Call<AsistenciaCreateResponse>
 
     @PUT("asistencia/{id}/")
     @FormUrlEncoded
--- app/src/main/java/com/example/maquetacion/ui/MainActivity5.kt
+++ app/src/main/java/com/example/maquetacion/ui/MainActivity5.kt
@@ -1,12 +1,72 @@
 package com.example.maquetacion.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
 import com.example.maquetacion.R
+import com.example.maquetacion.service.ApiService
+import retrofit2.Call
+import retrofit2.Response
+import android.widget.Toast
+import com.example.maquetacion.io.response.AsistenciaCreateResponse
+
 
 class MainActivity5 : AppCompatActivity() {
+
+    private val apiService: ApiService by lazy {
+        ApiService.create(this)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main5)
+
+        val imageView = findViewById<ImageView>(R.id.btn_regresar_vista)
+
+        imageView.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                val intent = Intent(this@MainActivity5, MainActivity4::class.java)
+                startActivity(intent)
+            }
+        }
+        )
+
+        val btnMarcar = findViewById<Button>(R.id.btnMarcar)
+        btnMarcar.setOnClickListener{
+            registerAsistencia()
+
+        }
     }
+    fun registerAsistencia(){
+        val callRegistreData = apiService.postAsistencia()
+
+        callRegistreData.enqueue(object : retrofit2.Callback<AsistenciaCreateResponse> {
+            override fun onFailure(call: Call<AsistenciaCreateResponse>, t: Throwable) {
+                Toast.makeText(
+                    this@MainActivity5,
+                    "Hubo un error en el servidor",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            override fun onResponse(call: Call<AsistenciaCreateResponse>, response: Response<AsistenciaCreateResponse>) {
+                if (response.isSuccessful) {
+                    val intent = Intent(this@MainActivity5, MainActivity6::class.java)
+                    startActivity(intent)
+                    println(response.body())
+                    finish()
+                } else {
+                    Toast.makeText(
+                        this@MainActivity5,
+                        "Las credenciales son incorrectas",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        })
+    }
+
+
 }
