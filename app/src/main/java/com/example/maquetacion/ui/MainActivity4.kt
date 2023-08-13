3befb8613e0ba5c29a0d347091b6430430c0f79b
sakitowea <albert_serpent_99@hotmail.com>
2023-08-13 01:06:03 -0500

    main_Activity_4

--- app/src/main/java/com/example/maquetacion/ui/MainActivity4.kt
+++ app/src/main/java/com/example/maquetacion/ui/MainActivity4.kt
@@ -1,13 +1,60 @@
 package com.example.maquetacion.ui
-
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import com.example.maquetacion.R
+import com.example.maquetacion.service.ApiService
+import com.example.maquetacion.model.User
+import com.example.maquetacion.model.login.LoginResponse
+import retrofit2.Call
+import retrofit2.Response
 
 class MainActivity4 : AppCompatActivity() {
+
+    private val apiService: ApiService by lazy {
+        ApiService.create(this)
+    }
+
+
+
+    fun perfomData(){
+        val callGetUserData = apiService.getUser()
+        val tvFirstName = findViewById<TextView>(R.id.tvFirstName)
+        val tvLastName = findViewById<TextView>(R.id.tvLastName)
+        val tvEmail = findViewById<TextView>(R.id.tvEmail)
+
+
+
+        callGetUserData.enqueue(object : retrofit2.Callback<User> {
+            override fun onFailure(call: Call<User>, t: Throwable) {
+                Toast.makeText(
+                    this@MainActivity4,
+                    "Hubo un error en el servidor",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            override fun onResponse(call: Call<User>, response: Response<User>) {
+                if (response.isSuccessful) {
+                    val user:User?=response.body()
+                    tvFirstName.setText(user?.first_name)
+                    tvLastName.setText(user?.last_name)
+                    tvEmail.setText(user?.email)
+                    println(user)
+                } else {
+                    Toast.makeText(
+                        this@MainActivity4,
+                        "Las credenciales son incorrectas",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        })
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +70,7 @@ class MainActivity4 : AppCompatActivity() {
         }
 
         setContentView(R.layout.activity_main4)
+        perfomData()
 
         val btnListAsistencia = findViewById<Button>(R.id.btnVerAsistencias)
         val btnMarcacion = findViewById<Button>(R.id.btnRealizarMarcacion)
