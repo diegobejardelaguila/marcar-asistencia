fd2ab861ef006ae96da42a43a59b36064c8f2b77
sakitowea <albert_serpent_99@hotmail.com>
2023-08-14 18:24:56 -0500

    cambios vistas

--- app/src/main/java/com/example/maquetacion/model/User.kt
+++ app/src/main/java/com/example/maquetacion/model/User.kt
@@ -1,10 +1,15 @@
 package com.example.maquetacion.model
 
 data class User(
-    val id: Int,
+    val id: Int?,
     val email: String,
-    val first_name: String,
-    val last_name: String,
-    val is_active: Boolean,
-    val is_staff: Boolean
+    val img_profile: String?,
+    val phone: String?,
+    val dni: String?,
+    val first_name: String?,
+    val last_name: String?,
+    val expected_entry_time: String?,
+    val expected_exit_time: String?,
+    val is_active: Boolean?,
+    val is_staff: Boolean?
 )
